2e4b0289eb95dbfad9fea427f0eed60c32ad06f3
Fabian Cremer <25341692+fabiancremer@users.noreply.github.com>
2026-03-09 13:43:50 +0100

    build without yarn

--- bin/build-production.sh
+++ bin/build-production.sh
@@ -5,4 +5,4 @@ export FV_STATIC_URL="https://friedensvertraege.ieg-mainz.de/friverplus"
 export FV_STATIC_ROOT="https://friedensvertraege.ieg-mainz.de/friverplus"
 export FV_STATIC_PREFIX='https://friedensvertraege.ieg-mainz.de'
 
-yarn run build
+./bin/build.sh build
