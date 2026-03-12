22f729bcf42914004595d951db84e103870b4c71
Moritz Schepp <schepp@wendig.io>
2026-03-12 10:54:16 -0700

    see #60

--- bin/build-production.sh
+++ bin/build-production.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -e
 
 export NODE_ENV=production
-export FV_STATIC_URL="https://friedensvertraege.ieg-mainz.de/friverplus"
-export FV_STATIC_ROOT="https://friedensvertraege.ieg-mainz.de/friverplus"
+export FV_STATIC_URL="https://friedensvertraege.ieg-mainz.de"
+export FV_STATIC_ROOT="https://friedensvertraege.ieg-mainz.de"
 export FV_STATIC_PREFIX='https://friedensvertraege.ieg-mainz.de'
+export FV_USE_LOCAL_IMAGES="true"
 
-yarn run build
+npm run build
