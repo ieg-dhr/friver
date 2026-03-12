a928cb3bc97daad3dec616226e439beb8c3ad948
Moritz Schepp <schepp@wendig.io>
2026-03-12 10:44:00 -0700

    see #59

--- rollup.config.js
+++ rollup.config.js
@@ -8,12 +8,30 @@ import riot from 'rollup-plugin-riot'
 import terser from '@rollup/plugin-terser'
 
 import fonts from './src/fonts.js'
+// import {pick} from './src/lib/util.js'
+
+const pick = (obj, keys) => {
+  const a = keys.
+    filter(key => key in obj).
+    map(key => [key, obj[key]])
+  
+  return Object.fromEntries(a)
+}
 
 const NODE_ENV = process.env['NODE_ENV'] || 'development'
+const env = Object.assign({NODE_ENV}, process.env)
+const senv = pick(env, [
+  'NODE_ENV',
+  'FV_STATIC_URL',
+  'FV_STATIC_PREFIX',
+  'FV_STATIC_ROOT',
+  'FV_LEGACY_URL',
+  'FV_USE_LOCAL_IMAGES'
+])
+
+fs.writeFileSync('./.env.js', 'export default ' + JSON.stringify(senv))
+
 const optimize = (NODE_ENV == 'production')
-
-fs.writeFileSync('./.env.js', 'export default ' + JSON.stringify(process.env))
-
 const tpl1 = fs.readFileSync('./src/index.ejs', {encoding: 'utf8'})
 const html1 = ejs.render(tpl1, {fonts})
 fs.writeFileSync('./public/index.html', html1)
